85d06b5387dd9a2afb3a3c04659dd0a6ca18ef90
Qivikavi <61878419+Qivikavi@users.noreply.github.com>
2021-01-29 15:23:09 +1000

    Добавьте файлы проекта.

---
+++ ExtraTask1/ExtraTask1/AllColors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
+using System.Text;
+
+namespace ExtraTask1
+{
+    public static class AllColors
+    {
+        public static List<Color> Getallcolors()
+        {
+            List<Color> allColors = new List<Color>();
+
+            foreach (PropertyInfo property in typeof(Color).GetProperties())
+            {
+                if (property.PropertyType == typeof(Color))
+                {
+                    allColors.Add((Color)property.GetValue(null));
+                }
+            }
+            return allColors;
+        }
+    }
+}
---
+++ ExtraTask1/ExtraTask1/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace ExtraTask1
+{
+    public class Colors
+    {
+        public string NameColor { get; set; }
+    }
+    public partial class MainPage : ContentPage
+    {
+        public List<Colors> allcolors { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            allcolors = new List<Colors>();
+            foreach (System.Drawing.Color color in AllColors.Getallcolors())
+            {
+                allcolors.Add(new Colors()
+                {
+                    NameColor = color.Name
+                });
+            }
+            BindingContext = this;
+        }
+    }
+}
